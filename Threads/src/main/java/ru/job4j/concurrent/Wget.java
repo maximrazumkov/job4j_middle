f574102fa0e8a5304a7cf3c35f2a87daf6cb8be8
Maxim Razumkov <maxim.razumkov@gmail.com>
2020-04-26 16:31:07 +0300

    1.2. Режим ожидания.[#279448]

---
+++ Threads/src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,19 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    for (int i = 0; i < 101; ++i) {
+                        try {
+                            Thread.sleep(1000);
+                            System.out.print("\rLoading : " + i + "%");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        thread.start();
+    }
+}
