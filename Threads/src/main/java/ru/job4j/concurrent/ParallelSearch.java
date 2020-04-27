bca2df36dfb6df892a4d98a9e5a1b091b39a44a1
Maxim Razumkov <maxim.razumkov@gmail.com>
2020-04-27 13:26:21 +0300

    2. Обеспечить остановку потребителя.[#279441]

---
+++ Threads/src/main/java/ru/job4j/concurrent/ParallelSearch.java
@@ -0,0 +1,39 @@
+package ru.job4j.concurrent;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) throws Exception {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        if (Thread.currentThread().isInterrupted()) {
+                            break;
+                        }
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        Thread produce = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        produce.start();
+        produce.join();
+        consumer.interrupt();
+    }
+}
