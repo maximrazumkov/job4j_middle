1ed02e219ab822e62590ed766d2c7aafe6e23ef2
Maxim Razumkov <maxim.razumkov@gmail.com>
2020-05-06 18:53:09 +0300

    0. CAS - операции[#279469]

--- Threads/src/main/java/ru/job4j/concurrent/CASCount.java
+++ Threads/src/main/java/ru/job4j/concurrent/CASCount.java
@@ -3,16 +3,5 @@ package ru.job4j.concurrent;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class CASCount {
-    private AtomicReference<Integer> count = new AtomicReference<>();
 
-    public int getCount() {
-        return count.get();
-    }
-
-    public void increment() {
-        Integer elem;
-        do {
-            elem = count.get();
-        } while (count.compareAndSet(elem, ++elem));
-    }
 }
